120cc119055d3c89f39a1490a85e9eec9999c199
조경진 <kj_cho@idealidea.co.kr>
2020-08-24 21:44:47 +0900

    로그인 버튼 눌리면 입력한 id/pw 알아내기

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.daily10minutes_20200824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +14,15 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+//            이 받아낸 id/pw을 어떻게 서버에 로그인 확인으로 요청하는가?
+
+        }
 
     }
 
