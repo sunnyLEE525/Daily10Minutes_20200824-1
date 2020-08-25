996c8a5830de4c478608ecd2190b4f94f3d4588e
조경진 <kj_cho@idealidea.co.kr>
2020-08-25 21:59:16 +0900

    중복 확인 결과를 파싱해서 화면에 반영

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/SignUpActivity.kt
@@ -2,6 +2,9 @@ package kr.co.tjoeun.daily10minutes_20200824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -13,6 +16,33 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        emailCheckBtn.setOnClickListener {
+
+//            입력된이메일 확인 => 해당 이메일을 서버에 중복검사기능 요청 => 결과에 따른 문구 반영
+
+            val inputEmail = signUpEmailEdt.text.toString()
+
+            ServerUtil.getRequestEmailCheck(inputEmail, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+                    val code = json.getInt("code")
+
+                    runOnUiThread {
+                        if (code == 200) {
+                            emailCheckResultTxt.text = "사용해도 좋은 이메일 입니다."
+                        }
+                        else {
+                            emailCheckResultTxt.text = "중복된 이메일입니다."
+                        }
+                    }
+
+
+
+                }
+
+            })
+
+        }
 
     }
 
--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.daily10minutes_20200824.utils
 
 import android.util.Log
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.json.JSONObject
 import java.io.IOException
 
@@ -74,6 +75,48 @@ class ServerUtil {
 
         }
 
+        fun getRequestEmailCheck(emailAddress : String, handler: JsonResponseHandler?) {
+
+//            서버에 Request를 날려주는 클라이언트 역할을 돕는 변수
+            val client = OkHttpClient()
+
+//            url (호스트주소+기능주소)을 만드는 과정에서 => 필요 파라미터도 가공-첨부
+            val urlBuilder = "${BASE_URL}/email_check".toHttpUrlOrNull()!!.newBuilder()
+//            url 가공기를 이용해서 => 필요 데이터 첨부
+            urlBuilder.addEncodedQueryParameter("email", emailAddress)
+
+//            가공이 끝난 url을 => urlStr 으로 완성
+            val urlStr = urlBuilder.build().toString()
+
+//            임시 : 어떻게 url이 가공되었는지 로그로 확인
+            Log.d("완성된url", urlStr)
+
+
+//            요청 정보를 담는 request
+            val request = Request.Builder()
+                .url(urlStr)
+                .get()
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val bodyString = response.body!!.string()
+
+                    val json = JSONObject(bodyString)
+
+                    Log.d("서버응답본문", json.toString())
+
+                    handler?.onResponse(json)
+                }
+
+            })
+
+
+        }
 
     }
 
