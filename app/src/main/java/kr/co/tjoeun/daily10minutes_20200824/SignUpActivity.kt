ae67d5644a5aca8477f091548874f8cbe48df89e
조경진 <kj_cho@idealidea.co.kr>
2020-08-25 22:06:10 +0900

    이메일 문구 변경 이벤트 -> 변경되면 무조건 중복확인을 다시 요구.

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/SignUpActivity.kt
@@ -2,6 +2,9 @@ package kr.co.tjoeun.daily10minutes_20200824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
 import org.json.JSONObject
@@ -16,6 +19,25 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        이메일 입력칸의 내용이 변경된 경우 => 중복검사를 다시 하도록 유도
+
+        signUpEmailEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("입력문구", p0.toString())
+
+                emailCheckResultTxt.text = "중복 확인을 해주세요."
+            }
+
+        })
 
         emailCheckBtn.setOnClickListener {
 
