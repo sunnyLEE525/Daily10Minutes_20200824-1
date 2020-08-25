a2cb0e42394b189477ae4b18e99aff57fd721488
조경진 <kj_cho@idealidea.co.kr>
2020-08-25 20:09:44 +0900

    서버가 돌려준 응답을 메인화면에서 확인

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -2,8 +2,10 @@ package kr.co.tjoeun.daily10minutes_20200824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -23,7 +25,16 @@ class MainActivity : BaseActivity() {
 
 //            이 받아낸 id/pw을 어떻게 서버에 로그인 확인으로 요청하는가?
 
-            ServerUtil.postRequestLogin(inputId, inputPw)
+            ServerUtil.postRequestLogin(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+//                    실제로 응답이 돌아왔을때 실행시켜줄 내용
+
+                    Log.d("메인화면에서 응답확인", json.toString())
+
+                }
+
+            })
 
         }
 
--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
@@ -7,6 +7,11 @@ import java.io.IOException
 
 class ServerUtil {
 
+//    화면(액티비티)의 입장에서, 서버응답이 돌아왔을때 실행해줄 내용을 담기 위한 인터페이스
+    interface JsonResponseHandler {
+        fun onResponse(json : JSONObject)
+    }
+
     companion object {
 
 //        이 영역 안에 만드는 변수 or 함수는 객체를 이용하지 않고,
@@ -17,7 +22,7 @@ class ServerUtil {
 
 //        로그인 기능 => 로그인을 수행하는 함수 작성
 
-        fun postRequestLogin(id : String, pw: String) {
+        fun postRequestLogin(id : String, pw: String, handler: JsonResponseHandler?) {
 
 //            안드로이드 앱이 클라이언트로 동작하도록 도와주자.
             val client = OkHttpClient()
@@ -57,6 +62,11 @@ class ServerUtil {
                     val json = JSONObject(bodyString)
 
                     Log.d("서버응답본문", json.toString())
+
+//                    어떤 처리를 해줄지 가이드북 (인터페이스) 이 존재한다면,
+//                    그 가이드북에 적힌 내용을 실제로 실행
+
+                    handler?.onResponse(json)
 
                 }
 
