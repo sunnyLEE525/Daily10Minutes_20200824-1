08ff8246613679058f22a9c0cbce37128ab0f8d5
조경진 <kj_cho@idealidea.co.kr>
2020-08-26 21:53:26 +0900

    로그인 사용자 토큰 저장/조회 기능 추가

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ContextUtil.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ContextUtil.kt
@@ -9,6 +9,7 @@ class ContextUtil {
         private val prefName = "Daily10MinutesPref"
 
         private val AUTO_LOGIN_CHECK = "AUTO_LOGIN_CHECK"
+        private val LOGIN_USER_TOKEN = "LOGIN_USER_TOKEN"
 
         fun setAutoLoginCheck(context: Context, isAuto : Boolean) {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
@@ -18,6 +19,16 @@ class ContextUtil {
         fun getAutoLoginCheck(context: Context) : Boolean {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             return pref.getBoolean(AUTO_LOGIN_CHECK, false)
+        }
+
+        fun setLoginUserToken(context: Context, token: String) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putString(LOGIN_USER_TOKEN, token).apply()
+        }
+
+        fun getLoginUserToken(context: Context) : String {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getString(LOGIN_USER_TOKEN, "")!!
         }
 
     }
