c837bfc0f9233e2896d8efc6972d3d39e7df2b6b
조경진 <kj_cho@idealidea.co.kr>
2020-08-27 19:59:41 +0900

    메인화면에서 => 프로젝트 목록을 서버에 요청

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -2,8 +2,13 @@ package kr.co.tjoeun.daily10minutes_20200824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.daily10minutes_20200824.datas.Project
+import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mProjectList = ArrayList<Project>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +23,16 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+        getProjectListFromServer()
+    }
+
+    fun getProjectListFromServer() {
+        ServerUtil.getRequestProjectList(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+            }
+
+        })
     }
 
 }
