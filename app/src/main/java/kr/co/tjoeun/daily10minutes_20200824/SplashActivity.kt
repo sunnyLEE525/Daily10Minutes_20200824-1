5a06080866b0ab81cc74acbf318589781c85ba6a
조경진 <kj_cho@idealidea.co.kr>
2020-08-27 21:58:51 +0900

    2.5초 지난 후에 상황별로 가야할 화면을 다르게 지정   => 자동 로그인 기능

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/SplashActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/SplashActivity.kt
@@ -1,7 +1,11 @@
 package kr.co.tjoeun.daily10minutes_20200824
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kr.co.tjoeun.daily10minutes_20200824.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
 
@@ -17,6 +21,32 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+//            자동로그인 된 상황? => 메인화면으로
+//             자동로그인 체크박스 true AND 로그인도 되었어야
+//            로그인 필요? => 로그인 화면으로
+
+            val myIntent : Intent
+
+            if (ContextUtil.getAutoLoginCheck(mContext) && ContextUtil.getLoginUserToken(mContext) != "") {
+//                자동 로그인 OK
+                myIntent = Intent(mContext, MainActivity::class.java)
+            }
+            else {
+//                로그인 필요
+                myIntent = Intent(mContext, LoginActivity::class.java)
+            }
+
+            startActivity(myIntent)
+            finish()
+
+        }, 2500)
+
+
 
     }
 
