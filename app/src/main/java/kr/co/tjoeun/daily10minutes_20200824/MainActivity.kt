ed4109aaaa6b8576af54a0aae991c583d904a7f6
조경진 <kj_cho@idealidea.co.kr>
2020-08-27 21:14:13 +0900

    목록에 담긴 프로젝트 목록 => 리스트뷰에 출력  실 데이터 반영 X

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -3,6 +3,8 @@ package kr.co.tjoeun.daily10minutes_20200824
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.daily10minutes_20200824.adapters.ProjectAdapter
 import kr.co.tjoeun.daily10minutes_20200824.datas.Project
 import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
 import org.json.JSONObject
@@ -10,6 +12,8 @@ import org.json.JSONObject
 class MainActivity : BaseActivity() {
 
     val mProjectList = ArrayList<Project>()
+
+    lateinit var mProjectAdapter : ProjectAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,10 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
         getProjectListFromServer()
+
+        mProjectAdapter = ProjectAdapter(mContext, R.layout.project_list_item, mProjectList)
+        projectListView.adapter = mProjectAdapter
+
     }
 
     fun getProjectListFromServer() {
@@ -47,6 +55,14 @@ class MainActivity : BaseActivity() {
                     project.description = projectObj.getString("description")
 
                     mProjectList.add(project)
+                }
+
+//                비동기처리 => 어댑터 연결이 먼저 끝나고, 프로젝트목록이 나중에 추가될 수 있다.
+//                리스트뷰의 입장에서는 => 내용물 목록이 변경된 상황. => notifyDataSetChanged()로 새로고침.
+//                새로고침 : UI에 영향을 주겠다. => runOnUiThread {  } 안에 작성해야 앱이 동작함.
+
+                runOnUiThread {
+                    mProjectAdapter.notifyDataSetChanged()
                 }
 
             }
