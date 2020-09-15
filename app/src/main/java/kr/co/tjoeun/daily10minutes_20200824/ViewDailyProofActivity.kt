33652fdb04bff81f2367d059bc93b61ad4631ee8
조경진 <kj_cho@idealidea.co.kr>
2020-09-15 22:19:47 +0900

    날짜 선택버튼 => DatePickerDialog 출현  - 월의 경우에는 0~11월로 생각하고 처리.  - Calendar.영어월이름 으로 대체

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewDailyProofActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewDailyProofActivity.kt
@@ -1,7 +1,11 @@
 package kr.co.tjoeun.daily10minutes_20200824
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_view_daily_proof.*
+import java.util.*
 
 class ViewDailyProofActivity : BaseActivity() {
 
@@ -13,6 +17,23 @@ class ViewDailyProofActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        selectDateBtn.setOnClickListener {
+
+            val datePickerDialog = DatePickerDialog(mContext, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+//                날짜가 선택되면 실행해줄 코드
+
+                Log.d("선택된 년", year.toString())
+                Log.d("선택된 월", month.toString())
+                Log.d("선택된 일", dayOfMonth.toString())
+
+//                실생활 : 1~12월
+//                JAVA / Kotlin : 0~11월
+
+            }, 2020, Calendar.JUNE, 9)
+            datePickerDialog.show()
+
+        }
 
     }
 
