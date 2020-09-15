10d328d38d3a45767c8f745449b7de381e702f91
조경진 <kj_cho@idealidea.co.kr>
2020-09-15 20:36:17 +0900

    사용자 표현 클래스 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/datas/User.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.daily10minutes_20200824.datas
+
+class User {
+
+    var id = 0
+    var email = ""
+    var nickName = ""
+
+    val profileImageArrayList = ArrayList<String>()
+
+}
