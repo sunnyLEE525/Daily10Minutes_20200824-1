6401f63de200fdab97b3e3cce95805d936eb326d
조경진 <kj_cho@idealidea.co.kr>
2020-09-15 21:21:45 +0900

    사용자 객체를 Json기반으로 만드는 함수 작성중  => 프로필 사진 목록을 파싱해야하는 상황.

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/datas/User.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/datas/User.kt
@@ -1,5 +1,7 @@
 package kr.co.tjoeun.daily10minutes_20200824.datas
 
+import org.json.JSONObject
+
 class User {
 
     var id = 0
@@ -7,5 +9,25 @@ class User {
     var nickName = ""
 
     val profileImageArrayList = ArrayList<String>()
+
+    companion object {
+
+        fun getUserFromJson(json: JSONObject) : User {
+
+//            기본값세팅된 User 만들고 => json변수를 이용해 내용물 채우고 => 결과로 return
+
+            val user = User()
+
+//            json으로 user변수의 항목들을 채우자.
+            user.id = json.getInt("id")
+            user.email = json.getString("email")
+            user.nickName = json.getString("nick_name")
+
+//            사용자의 프사 목록도 파싱해야함.
+
+            return user
+        }
+
+    }
 
 }
