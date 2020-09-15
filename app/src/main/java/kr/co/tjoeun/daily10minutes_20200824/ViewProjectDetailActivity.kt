051dd0577bee5b6ceb8b0eb05785f3836d8f6cf9
조경진 <kj_cho@idealidea.co.kr>
2020-09-15 19:40:20 +0900

    프로젝트 신청 상태에따른 버튼 활성화 처리

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewProjectDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewProjectDetailActivity.kt
@@ -52,6 +52,25 @@ class ViewProjectDetailActivity : BaseActivity() {
                 runOnUiThread {
                     proofMethodTxt.text = mProject.proofMethod
                     onGoingMemberCountTxt.text = "(현재 참여 인원 : ${mProject.onGoingMemberCount}명)"
+
+//                    myLastStatus => 마지막으로 변경된 프로젝트 신청 상태
+//                    null : 신청한적이 없다.
+//                    ONGOING : 신청해서 진행중
+//                    FAIL : 중도포기 or 3일 연속  인증글 X (자동 포기)
+//                    COMPLETE : 66(프로젝트마다 다름)일 짜리 모두 수행 완료.
+
+//                    신청하기 언제? 그 외의 모든 상황.
+//                    중도 포기 언제? 상태가 ONGOING일때.
+
+                    if (mProject.myLastStatus == "ONGOING") {
+                        giveUpBtn.isEnabled = true
+                        applyBtn.isEnabled = false
+                    }
+                    else {
+                        giveUpBtn.isEnabled = false
+                        applyBtn.isEnabled = true
+                    }
+
                 }
 
             }
