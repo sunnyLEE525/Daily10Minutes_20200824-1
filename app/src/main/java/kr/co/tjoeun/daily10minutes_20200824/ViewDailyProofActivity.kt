1e858ee0939e57f2fe23397ca4c6facd73eed98a
SunyoungLee <sunny_525@naver.com>
2020-09-16 19:49:37 +0900

    선택된 날짜 세팅 코드 작성 -> 세팅된 날짜를 꺼내오는 코드는 미작성. (get 으로 꺼내도 월이 1 작게 나옴)

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewDailyProofActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewDailyProofActivity.kt
@@ -21,11 +21,28 @@ class ViewDailyProofActivity : BaseActivity() {
         selectDateBtn.setOnClickListener {
 
             val datePickerDialog = DatePickerDialog(mContext, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-//                날짜가 선택되면 실행해줄 코드
 
-                Log.d("선택된 년", year.toString())
-                Log.d("선택된 월", month.toString())
-                Log.d("선택된 일", dayOfMonth.toString())
+//                일단 Calendar 변수를 하나 생성, calendar 객체를 담아두자.
+                val selectedDate = Calendar.getInstance()
+
+//                selectedDate에는 몇년 몇월 몇일이 들어있나? 기본값 : 현재 일시가 자동으로 기록됨.
+
+
+//                날짜가 선택되면 실행해줄 코드
+//                => 연도/월/일 각각의 항목을 변경(SelectedDate 변수의 값 변경)
+
+                selectedDate.set(Calendar.YEAR, year)
+                selectedDate.set(Calendar.MONTH, month)
+                selectedDate.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                //                년/월/일을 한꺼번에 세팅.
+                selectedDate.set(year, month, dayOfMonth)
+
+                Log.d("선택된 년", selectedDate.get(Calendar.YEAR).toString())
+                Log.d("선택된 월", selectedDate.get(Calendar.MONTH).toString())
+                Log.d("선택된 일", selectedDate.get(Calendar.DAY_OF_MONTH).toString())
+
+
 
 //                실생활 : 1~12월
 //                JAVA / Kotlin : 0~11월
