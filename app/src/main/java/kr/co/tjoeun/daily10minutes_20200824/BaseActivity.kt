edc33f14bb90f8cccc7d09e3b4d3004a8be4bf99
SunyoungLee <sunny_525@naver.com>
2020-09-17 20:41:37 +0900

    BaseActivity를 활용 => 모든 액션바가 있는 화면에서 액션바를 커스터마이징 적용

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/BaseActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/BaseActivity.kt
@@ -1,6 +1,9 @@
 package kr.co.tjoeun.daily10minutes_20200824
 
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -8,5 +11,38 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        액션바를 꺼내올 수 있는 상황일때는 => 커스텀 액션바로 세팅
+//        if (supportActionBar != null) {
+//            setCustomActionBar()
+//        }
+
+//        코틀린 문법으로 액션바를 커스텀 액션바로 세팅
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
+
+    }
+
+    fun setCustomActionBar() {
+        //        이 화면에 달린 액션바가 절대 null이 아니라고 하면서 가져옴.
+        val myActionBar = supportActionBar!!
+
+//        액션바를 커스텀으로 사용할 수 있도록 세팅
+        myActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+//        액션에 보여줄 커스텀으로 그린 화면 배치
+        myActionBar.setCustomView(R.layout.my_custom_actionbar)
+
+        //        커스텀액션바 뒤의 기본 색 제거 => 액션바를 들고 있는 툴바의 좌우 여백을 0으로 설정하자.
+        val parentToolBar = myActionBar.customView.parent as Toolbar
+        parentToolBar.setContentInsetsAbsolute(0,0)
+    }
+
+
+
 
 }
--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -27,20 +27,6 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
-
-//        이 화면에 달린 액션바가 절대 null이 아니라고 하면서 가져옴.
-        val myActionBar = supportActionBar!!
-
-//        액션바를 커스텀으로 사용할 수 있도록 세팅
-        myActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-
-//        액션에 보여줄 커스텀으로 그린 화면 배치
-        myActionBar.setCustomView(R.layout.my_custom_actionbar)
-
-//        커스텀액션바 뒤의 기본 색 제거 => 액션바를 들고 있는 툴바의 좌우 여백을 0으로 설정하자.
-        val parentToolBar = myActionBar.customView.parent as Toolbar
-        parentToolBar.setContentInsetsAbsolute(0,0)
-
     }
 
     override fun setupEvents() {
